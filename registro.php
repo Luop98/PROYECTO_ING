431bd90c5eda83ac1cba5cb42100677d6af5e63e
Poul <anrivandloup@gmail.com>
2022-10-17 23:09:12 -0500

    commit registro

---
+++ registro.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<body>
+ <h2>Registro de Miembros</h2>
+ <form method="POST" action="registro.php">
+ Nombre de Usuario: <br />
+ <input type="text" name="nombre" size="50" />
+ <p>
+ Contraseña: <br />
+ <input type="text" name="contraseña" size="50" />
+ <p>
+  Email: <br />
+ <input type="text" name="email" size="50" />
+ <p>
+ Codigo Usuario: <br />
+ <input type="text" name="codigo usuario" size="8"/>
+ <p>
+
+
+ <input type="submit" name="submit" value="Enviar" />
+ </form>
+ </body>
+</body>
+</html>
