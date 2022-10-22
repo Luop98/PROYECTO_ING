6f9f888b6a7091f6812fb97600b38553b4ac0987
Poul <anrivandloup@gmail.com>
2022-10-22 16:22:06 -0500

    formlario de tareas

---
+++ insertaT.php
@@ -0,0 +1,13 @@
+<?php
+include("conexion.php");
+
+//CAPTURAR VARIABLES DEL FORMULARIO DE TAREAS
+//$id=$_POST['id'];
+$tareas=$_POST['tareas'];
+
+$insert  = "inser into t_tareas(id,tareas) values('$id','$tareas')";
+$res = mysqli_query($conexion,$insert);
+
+if($res){
+    header("Location:inicio.php");
+}
