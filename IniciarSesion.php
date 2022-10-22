ae44b57d567dc0b45867e6b0b9b13942fabe4978
Poul <anrivandloup@gmail.com>
2022-10-22 00:18:21 -0500

    modificacion

--- IniciarSesion.php
+++ IniciarSesion.php
@@ -1,18 +1,24 @@
 <?php   
+    //inicializar la variable de sesion
     session_start();
+    // Llamar la conexion 
     include('Conexion.php');
 
+        // CONTENER EL USUARIO Y LA CLAVE
     if (isset($_POST['Usuario']) && isset($_POST['Clave']) ) {
 
+        //FUNCION PARA VALIDAR
     function validate($data){
         $data = trim($data);
         $data = stripslashes($data);
         $data = htmlspecialchars($data);
         return $data;
     }
-
+    // CREACION Y VALIDACION DE VARIACLES DE USUARIO Y CLAVE
     $Usuario = validate($_POST['Usuario']); 
     $Clave = validate($_POST['Clave']);
+
+    //creacion de validaciones de USUARIO Y CLAVE
 
     if (empty($Usuario)) {
         header("Location: Index.php?error=El Usuario Es Requerido");
@@ -22,10 +28,12 @@
         exit();
     }else{
 
-        // $Clave = md5($Clave);
-        
+       
+        //CONSULTA DE MYSQL 
         $Sql = "SELECT * FROM t_usuarios where NOMBRE_U='$Usuario' and CONTRASEÑA='$Clave'";
+        //EJECUTAR LA CONSULTA DE MYSQL
         $result = mysqli_query($conexion, $Sql);
+
 
         if(mysqli_num_rows($result) === 1){
             $row = mysqli_fetch_assoc($result);
