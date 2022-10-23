27918b6aad764d4af9ec0490a68513a9c01b0d20
Poul <anrivandloup@gmail.com>
2022-10-23 13:10:05 -0500

    Modificado

--- actualizar.php
+++ actualizar.php
@@ -16,22 +16,27 @@ $row=mysqli_fetch_array($query);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href ="StyleA.css">
     <title>ACTUALIZAR</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
 </head>
 <body>
-    <h3>ACTUALIZAR</h3>
+
+    <h3> <center> <strong>ACTUALIZAR</strong> </center> </h3>
+
 <form action="update.php" method="POST">
+
 <div class="container-mt-5">
     <form action="update.php" method="POST">
         <input type="hidden" name="id" value="<?php echo $row['id'] ?> ">
+        <label > <strong>TAREA</strong> </label>
         <br>
-        <input type="text" class="form-control mb-3" name="tareas" placeholder="Tareas" value= "<?php echo $row['tareas'] ?>">
+        <input type="text" class="form-control mb-3" name="tareas" placeholder="Tarea" value= "<?php echo $row['tareas'] ?>">
 
-        <input type="submit" class="btn btn-prymary btn-block" value="Actualizar">
+ <input type="submit" class="btn btn-prymary btn-block" value="Actualizar"> 
     </form>
-
+        
 </div>
  </form>
 
